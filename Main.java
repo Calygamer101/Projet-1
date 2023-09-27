62a8621e9decdb259ccb75d6d0dbcf48435d9127
Calygamer101 <128635552+Calygamer101@users.noreply.github.com>
2023-09-27 10:30:17 -0400

    commit2

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Ballon b1 = new Ballon();
+        Ballon2 b2 = new Ballon();
+
+        ballon.type("Ballon de soccer");
+        ballon2.type("Ballon de bascketball ")
+
+        ballon.couleur("Ballon noir")
+        ballon2.couleur("Ballon orange")
+
+        ballon.nombre("12 ballon")
+        ballon2.nombre("1 ballon")
+    }
+}
