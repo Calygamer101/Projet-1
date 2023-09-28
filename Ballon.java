d05cbc4b33137c945919646e62c42fe6b4e576f1
Calygamer101 <128635552+Calygamer101@users.noreply.github.com>
2023-09-28 13:46:03 -0400

    commit 3

--- Ballon.java
+++ Ballon.java
@@ -1,3 +1,22 @@
 public  class Ballon {
     
+    String type = "normal";
+    String couleur = "invisible";
+    int nombre = 0;
+
+
+    public void quelleType(String type) {
+        this.type = type;
+
+    }
+
+    public void quelleCouleur(String couleur){
+        this.couleur = couleur;
+    }
+        
+    public void combienBall(int nombre) {
+        this.nombre = nombre;
+
+    }
 }
+
--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         Ballon b1 = new Ballon();
-        Ballon2 b2 = new Ballon();
+        Ballon b2 = new Ballon();
 
-        ballon.type("Ballon de soccer");
-        ballon2.type("Ballon de bascketball ")
+        b1.quelleType("Ballon de soccer");
+        b2.quelleType("Ballon de bascketball ");
 
-        ballon.couleur("Ballon noir")
-        ballon2.couleur("Ballon orange")
+        b1.quelleCouleur("Ballon noir");
+        b2.quelleCouleur("Ballon orange");
 
-        ballon.nombre("12 ballon")
-        ballon2.nombre("1 ballon")
+        b1.combienBall(12);
+        b2.combienBall(1);
+
+        System.out.println("B1");
+        System.out.println("Type:" + b1.type);
+        System.out.println("Couleur:" + b1.couleur);
+        System.out.println("Nombre:" + b1.nombre);
+
+        System.out.println("B2");
+        System.out.println("Type:" + b2.type);
+        System.out.println("Couleur:" +b2.couleur);
+        System.out.println("Nombre:" +b2.nombre);
+        
     }
 }
